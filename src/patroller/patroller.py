add some desired behaviors (stopping when motion complete, recursive rotation calls to reach correct angles)

src/patroller/patroller.py
@@ -14,7 +14,7 @@ import tf  # library for transformations.
 
 from src.patroller.get_test_json_graph import get_test_json_graph
 from src.patroller.route_planner import RoutePlanner
-from src.utilities.get_current_position import get_current_position_map, translate_point_between_frames
+from src.utilities.get_current_position import get_current_position, translate_point_between_frames
 from src.utilities.move_to_point import move_to_point
 from src.utilities.parse_graph import parse_json_graph
 
@@ -83,39 +83,47 @@ class Patroller:
 
     def plan(self):
         """Makes a plan for traversing the graph"""
-        trans = get_current_position_map(self.transform_listener)
+        # gets the current transformation between the base_link and map reference frames
+        trans = get_current_position("map", "base_link", self.transform_listener)[0]
         planner = RoutePlanner(
             node_dictionary=self.node_dictionary,
             edge_dictionary=self.edge_dictionary,
             current_location={"x": trans[0], "y": trans[1]}
         )
         self.nodes_to_visit = planner.find_traversal()
+        self.should_plan = False
 
     def execute_plan(self):
         """Executes the plan for traversing the graph"""
         if len(self.nodes_to_visit) is 0:
             # re-plan if we just traversed the entire graph
-            self.plan()
+            print("Re-patrolling the graph")
+            return
+            # self.plan()
 
         next_node = self.nodes_to_visit.pop(0)
-        next_node_in_base_link = translate_point_between_frames(
-            point=next_node,
-            frame1="map",
-            frame2="base_link",
-            transform_listener=self.transform_listener
-        )
-        move_to_point(current_point={"x": 0, "y": 0},
-                      current_orientation=0,
-                      desired_point=next_node_in_base_link,
-                      move_cmd_pub=self.move_cmd_pub
+        trans, rot = get_current_position("map", "base_link", self.transform_listener)
+        yaw = tf.transformations.euler_from_quaternion(rot)[2]
+
+        print("Next node:")
+        print(next_node)
+        print("Current node:")
+        print(trans)
+        print("Orientation:")
+        print(yaw)
+        print("\n")
+
+        move_to_point(current_point=trans,
+                      current_orientation=yaw,
+                      desired_point=[next_node["x"], next_node["y"]],
+                      move_cmd_pub=self.move_cmd_pub,
+                      transform_listener=self.transform_listener
                       )
 
     def main(self):
         """Loops; triggers patrolling if mode is patrolling"""
         rate = rospy.Rate(FREQUENCY) # loop at 10 Hz.
         while not rospy.is_shutdown():
-            print(self.mode)
-
             if self.mode is "patrolling":
                 # if we haven't arrived on the graph yet, we will use the Restorer to get us there
                 if not self.is_on_graph:
@@ -123,8 +131,10 @@ class Patroller:
                     self.is_on_graph = True
 
                 if self.should_plan is True:
+                    print("Planning...")
                     self.plan()
                 else:
+                    print("Executing plan")
                     self.execute_plan()
 
             rate.sleep()

src/utilities/move_to_point.py
@@ -1,10 +1,14 @@
 import rospy  # module for ROS APIs
 from geometry_msgs.msg import Twist # message type for velocity command.
 import math
+import tf
+
+from src.utilities.get_current_position import get_current_position
 
 FREQUENCY = 10
 VELOCITY = 0.2 #m/s
 ANGULAR_VELOCITY = 0.5 #radians / sec
+ROTATION_ACCURACY_THRESHOLD = 0.1 # the tolerable rotation inaccuracy in radians
 
 
 def move(linear_vel, angular_vel, move_cmd_pub):
@@ -27,6 +31,8 @@ def run_movement_loop(movement_time, linear_vel, angular_vel, move_cmd_pub):
 
         move(linear_vel, angular_vel, move_cmd_pub)
         rate.sleep()
+    # stop when we've finished with the movement
+    move(0, 0, move_cmd_pub)
 
 
 def rotate(angle_to_rotate, move_cmd_pub):
@@ -40,7 +46,7 @@ def rotate(angle_to_rotate, move_cmd_pub):
     if reverse_traverse_direction:
         time_to_rotate = -time_to_rotate
         angular_velocity = -angular_velocity
-    run_movement_loop(time_to_rotate, 0, angular_velocity)
+    run_movement_loop(time_to_rotate, 0, angular_velocity, move_cmd_pub)
 
 
 def move_forward(distance, move_cmd_pub):
@@ -52,7 +58,7 @@ def move_forward(distance, move_cmd_pub):
 
 
 def get_distance_angle_between_points(desired_point, current_point, current_orientation):
-    """Gets the distance and angle between two points"""
+    """Gets the distance and angle between two points; points are in tuple/list notation"""
     difference = [desired_point[0] - current_point[0],
                   desired_point[1] - current_point[1]]
     # find the angle that we need to rotate to
@@ -76,15 +82,28 @@ def get_distance_angle_between_points(desired_point, current_point, current_orie
     # find the length that we need to travel
     length = math.sqrt(difference[0] ** 2 + difference[1] ** 2)
 
-    return length, angle_to_rotate
+    return length, angle_to_rotate, angle
 
 
-def rotate_to_point(current_orientation, current_point, desired_point, move_cmd_pub):
+def rotate_to_point(current_orientation, current_point, desired_point, move_cmd_pub, transform_listener):
     """
     Rotates in the direction of the specified point
     """
-    length, angle_to_rotate = get_distance_angle_between_points(desired_point, current_point, current_orientation)
+    length, angle_to_rotate, target_angle = get_distance_angle_between_points(
+        desired_point,
+        current_point,
+        current_orientation
+    )
+    print("Angle to rotate:")
+    print(angle_to_rotate)
+    print("\n")
     rotate(angle_to_rotate, move_cmd_pub)
+    trans, rot = get_current_position("map", "base_link", transform_listener)
+    yaw = tf.transformations.euler_from_quaternion(rot)[2]
+
+    # if we're not yet at the desired angle, call recursively until we are
+    if abs(target_angle - yaw) > ROTATION_ACCURACY_THRESHOLD:
+        rotate_to_point(yaw, current_point, desired_point, move_cmd_pub, transform_listener)
 
 
 def drive_to_point(current_point, desired_point, move_cmd_pub):
@@ -92,11 +111,11 @@ def drive_to_point(current_point, desired_point, move_cmd_pub):
     Drives to the point
     """
     # find the difference between the current point and the next one
-    length, angle_to_rotate = get_distance_angle_between_points(desired_point, current_point, 0)
+    length, angle_to_rotate, target_angle = get_distance_angle_between_points(desired_point, current_point, 0)
     move_forward(length, move_cmd_pub)
 
 
-def move_to_point(current_point, current_orientation, desired_point, move_cmd_pub):
+def move_to_point(current_point, current_orientation, desired_point, move_cmd_pub, transform_listener):
     """Moves the robot from the current point to the desired point"""
-    rotate_to_point(current_orientation, current_point, desired_point, move_cmd_pub)
+    rotate_to_point(current_orientation, current_point, desired_point, move_cmd_pub, transform_listener)
     drive_to_point(current_point, desired_point, move_cmd_pub)