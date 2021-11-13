0647b440b9a65d41701e18a07ea303a6e25cab86
sherry1iu <awesome.sherry.00@gmail.com>
2021-11-13 03:47:31 +0000

    Test detect

--- src/utilities/sherry_main.py
+++ src/utilities/sherry_main.py
@@ -1,37 +1,46 @@
 import rospy 
 
-
+FREQUENCY = 5 #Hz.
 
 # Testing for sherry nodes
 def main():
+    rospy.init_node("node")   
+    rate = rospy.Rate(FREQUENCY)
+
+
     while not rospy.is_shutdown():
         print("Placeholder for global localization. Robot is at (10, 10) on the occugrid.")
         x = 10
         y = 10
-        print("Robot is (fake) moving.")
+        print("Placeholder for moving. Robot is (fake) traversing graph.")
 
-        print("Robot is running LIDAR. Lidar both publishes and returns true/false")
+        print("Test for LIDAR. Lidar both publishes and returns true/false")
         
         ######### RUN LIDAR DETECT
 
         obstacle = True ##### Placeholder for now
         
         if obstacle:
-            print("Robot found obstacle.")
+            print("BOOLEAN: Robot found obstacle.")
 
             cam_count = 0
 
+            print("Test for Camera.")
+            print("Test for PID.")
             while cam_count < 5:
 
                 ########## RUN CAMERA
                 ########## RUN PID
+                
 
                 cam_count = cam_count + 1
+                print("Cam count " + str(cam_count))
         
         
         
-        print("Restart with localization.")
+        print("Restart cycle.")
         print("\n")
+        rate.sleep()
                 
 
 
